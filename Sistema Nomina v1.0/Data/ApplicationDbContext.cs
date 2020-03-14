197109f8866309bd4ad50fef6c9842fd879dc5df
irnh95 <ivan@harperdevelopment.net>
2020-03-14 15:22:35 -0700

    Add project files.

---
+++ Sistema Nomina v1.0/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sistema_Nomina_v1._0.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
