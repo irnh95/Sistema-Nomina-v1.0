c231d1425ab013d5906fefe25357c41add079895
irnh95 <ivan@harperdevelopment.net>
2020-03-15 17:23:28 -0700

    Added unit work pattern

---
+++ DAL/UnitWork/IUnitWork.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Repository;
+
+namespace DAL.UnitWork
+{
+    public interface IUnitWork
+    {
+
+        EmployeeRepo Employee { get; }
+        EmployeeRoleRepo EmployeeRole { get; }
+        void Commit();
+    }
+}
---
+++ DAL/UnitWork/UnitWork.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Data;
+using DAL.Repository;
+
+namespace DAL.UnitWork
+{
+    /// <summary>
+    /// adding the unit work pattern
+    /// </summary>
+    public class UnitWork : IUnitWork
+    {
+
+        private ApplicationDbContext _dbContext;
+        private EmployeeRepo _employee;
+        private EmployeeRoleRepo _employeeRole;
+
+
+        public UnitWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public EmployeeRepo Employee
+        {
+            get
+            {
+                return _employee ??
+                    (_employee = new EmployeeRepo(_dbContext));
+            }
+        }
+
+        public EmployeeRoleRepo EmployeeRole
+        {
+            get
+            {
+                return _employeeRole ??
+                    (_employeeRole = new EmployeeRoleRepo(_dbContext));
+            }
+        }
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
